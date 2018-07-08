7193f27b6b64367616b2d361ca6c502f87e4c8e0
Marcelo Guimaraes <celoguimaraes@gmail.com>
2018-07-08 09:30:58 -0300

    Adicionando o projeto

---
+++ src/main/java/br/com/pucminas/projeto/resource/AlunoRestController.java
@@ -0,0 +1,27 @@
+package br.com.pucminas.projeto.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.pucminas.projeto.model.Aluno;
+import br.com.pucminas.projeto.service.CadastroAlunoService;
+
+//@RestController
+//@RequestMapping("/alunosrest")
+public class AlunoRestController {
+
+	@Autowired
+	private CadastroAlunoService cadastroAlunoService;
+	
+	@GetMapping
+	public ResponseEntity<?> listar() {
+		List<Aluno> alunoList = cadastroAlunoService.findAll();
+		return !alunoList.isEmpty() ? ResponseEntity.ok(alunoList) : ResponseEntity.noContent().build();
+	}
+	
+}
